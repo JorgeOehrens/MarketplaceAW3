ec92134c784046f788f3e0d6783f26d9991e696d
Jorge Oehrens <jorge.oehrens@gmail.com>
2024-02-08 18:17:28 -0300

    Front-end Login

--- marketplace-assetsweb3/src/app/layout.tsx
+++ marketplace-assetsweb3/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import '@/app/ui/global.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,3 +21,4 @@ export default function RootLayout({
     </html>
   );
 }
+
---
+++ marketplace-assetsweb3/src/app/ui/login-form.tsx
@@ -0,0 +1,32 @@
+
+const LoginForm = () =>{
+
+
+    return <>
+    <div className=" bg-white px-10 py-20 rounded-3xl border-3 border-gray-100">
+        <h1 className="text-5xl font-semibold text-center m-4 ">
+            Welcome back!
+        </h1>
+        <p className="font-medium text-lg text-gray-500 mt-4">
+            Welcome back! Please connect in Assets Web 
+        </p>
+
+
+        <div className='mt-8'>
+            <div >
+                {/* <ConnectButton  /> */}
+
+            </div>
+
+        </div>
+
+    </div>
+
+    
+    
+    </>
+
+}
+
+
+export default LoginForm ;
